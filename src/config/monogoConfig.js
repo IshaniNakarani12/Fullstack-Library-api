047e7e4d63bb5aa4f8ac7f2eec46ab065c93dc76
IshaniNakarani12 <ishaninakarani12@gmail.com>
2024-06-11 13:20:40 +1000

    small changes

--- src/config/monogoConfig.js
+++ src/config/monogoConfig.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectMongoDB = async () => {
-  console.log(process.env.MONGO_URL);
   try {
     const con = await mongoose.connect(process.env.MONGO_URL);
 
